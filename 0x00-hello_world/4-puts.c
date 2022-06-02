c026e26adf97e2a7737f17451f9c1e91d741c8f3
okpisamichael <okpisamichael@gmail.com>
2022-06-02 06:59:38 -0700

    first task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include <stdio.h>
+include <stdio.h>
 
 /**
  *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
  *     */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
