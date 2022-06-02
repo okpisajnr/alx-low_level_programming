384ad2a6f6e9237f963f821ab2307fab29316382
okpisamichael <okpisamichael@gmail.com>
2022-06-02 08:57:41 -0700

    done with task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
  *  * main - Entry point
- *  * Description: prints "\"Programming is like building a multilingual puzzle"
- *  * Return: 0
+ *   * Description: prints "\"Programming is like building a multilingual puzzle"
+ *    * Return: 0
  *
  */
 int main(void)
