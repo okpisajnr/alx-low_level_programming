6e64a833acb70a7ab51daeb4c5ec621956369f64
okpisamichael <okpisamichael@gmail.com>
2022-06-02 08:34:36 -0700

    done with task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
-#include <stdio.h>
+nclude <stdio.h>
 #include <unistd.h>
 /**
- *  * main - Entry point
- *   * Return1:
-*
-*/
+ *  *  * main - Entry point
+ *   *   * Return:1
+ *    *   
+ */
 int main(void)
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 	return (1);
 }
