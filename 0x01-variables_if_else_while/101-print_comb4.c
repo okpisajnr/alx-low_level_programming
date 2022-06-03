10128c434733278bdedbd14288235052e84caf87
okpisamichael <okpisamichael@gmail.com>
2022-06-03 06:56:41 -0700

    done with task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
+nclude <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 	int d, p, o;
@@ -13,19 +13,19 @@ int main(void)
 	{
 		for (p = d + 1; p <= '9'; p++)
 		{
-    			for (o = p +1; o <= '9'; o++)
-        		{
-    				if (p != d && p != o)
-       				{
-      					putchar(d);
-       					putchar(p);
-  					putchar(o);
+			for (o = p +1; o <= '9'; o++)
+			{
+				if (p != && p != o)
+			        {
+					putchar(d);
+				    	putchar(p);
+					putchar(o);
 					if (d == '7' && p == '8' && o == '9')
 						continue;
 					putchar(',');
 					putchar(' ');
-			        }
-		        }
+				}
+	                }
 		}
 	}
 	putchar('\n');
