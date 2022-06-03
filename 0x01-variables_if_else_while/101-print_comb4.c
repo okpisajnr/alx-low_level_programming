7667c5059397ddc45b7f51f5792a6b7431520214
okpisamichael <okpisamichael@gmail.com>
2022-06-03 07:11:44 -0700

    done with task 12

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,14 +9,15 @@
 int main(void)
 {
 	int d, p, o;
+	
 	for (d = '0'; d < '9'; d++)
 	{
 		for (p = d + 1; p <= '9'; p++)
 		{
-			for (o = p +1; o <= '9'; o++)
+			for (o = p + 1; o <= '9'; o++)
 			{
 				if (p != d && p != o)
-			        {
+				{
 					putchar(d);
 				    	putchar(p);
 					putchar(o);
