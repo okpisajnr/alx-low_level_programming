9c1844b710ff416e7c2174dec1a23b76274627b1
okpisa felix michael <okpisamichael@gmail.com>
2022-06-09 08:05:14 -0700

    task o completed

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef main.h 
-#define main.h
+#include <stdio.h>
 
 int _putchar(char c);
 int _isupper(int c);
@@ -13,5 +12,3 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
-#endif
