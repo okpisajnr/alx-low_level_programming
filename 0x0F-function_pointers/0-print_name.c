2cea6a5069b0690b78ccabfc06a4ea995f1518f5
okpisa felix michael <okpisamichael@gmail.com>
2022-07-07 07:31:12 -0700

    hot fix

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,13 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - funtion entry point
- * @name: str to print
- * @f: function pointer
- * Desc: a function that prints a name
- *
- * Return: void
- */
+ *  * print_name - funtion entry point
+ *   * @name: str to print
+ *    * @f: function pointer
+ *     * Desc: a function that prints a name
+ *      *
+ *       * Return: void
+ *        */
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
