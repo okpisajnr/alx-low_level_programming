1e46fd8d20a0dbf0fd38a396bd0b487fc132508f
okpisa felix michael <okpisamichael@gmail.com>
2022-07-07 22:41:17 -0700

    done

--- 0x0F-function_pointers/0-main.c
+++ 0x0F-function_pointers/0-main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include "function_pointers.h"
 
+/**
+ * print_name_as_is - prints a name as is
+ * @name: name of the person
+ *
+ * Return: Nothing
+ */
 void print_name_as_is(char *name)
 {
 	printf("Hello, my name is %s\n", name);
 }
-
+/**
+ * print_name_uppercase - print a name in uppercase
+ * @name: name of the person
+ *
+ * Return: Nothing
+ */
 void print_name_uppercase(char *name)
 {
 	unsigned int i;
@@ -26,6 +37,11 @@ void print_name_uppercase(char *name)
 	}
 }
 
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	print_name("Bob", print_name_as_is);
