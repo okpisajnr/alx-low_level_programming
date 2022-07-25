ec88c101136cfa5db2cdaac065b8ef9edc8fb520
okpisa felix michael <okpisamichael@gmail.com>
2022-07-25 06:57:48 -0700

    done with task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,12 +2,12 @@
 #include <stdio.h>
 
 /**
- *  * error_file - checks if files can be opened.
- *   * @file_from: file_from.
- *    * @file_to: file_to.
- *     * @argv: arguments vector.
- *      * Return: no return.
- *       */
+ * error_file - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
 void error_file(int file_from, int file_to, char *argv[])
 {
 	if (file_from == -1)
@@ -23,11 +23,11 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- *  * main - check the code for Holberton School students.
- *   * @argc: number of arguments.
- *    * @argv: arguments vector.
- *     * Return: Always 0.
- *      */
+ * main - check the code for Holberton School students.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, err_close;
